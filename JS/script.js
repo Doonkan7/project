15d024ee962a1966b8d3db3b38b1788c98c4b04e
Doonkan <michayloff@gmail.com>
2023-01-02 11:49:13 +0500

    add lesson 21

--- JS/script.js
+++ JS/script.js
@@ -10,8 +10,8 @@
 
 const answers = [];
 
-answers[0] = prompt('Как ваше имя?', '');
-answers[1] = prompt('Как ваше фамилия?', '');
-answers[2] = prompt('Сколько  вам лет?', '');
+// answers[0] = prompt('Как ваше имя?', '');
+// answers[1] = prompt('Как ваше фамилия?', '');
+// answers[2] = prompt('Сколько  вам лет?', '');
 
-document.write(answers);
+console.log(typeof(answers));

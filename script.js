90227aa8da895db23d77bfe273066a696e087b90
Doonkan <michayloff@gmail.com>
2023-01-04 20:14:50 +0500

    dodyfication

---
+++ script.js
@@ -0,0 +1,26 @@
+"use strict";
+// Автоматизация вопросов пользователя о фильмах при помощи цикла
+
+const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
+
+for (let i = 0; i < 2; i++) {
+    const a = prompt('Один из последних просмотренных фильмов?', ''),
+          b = prompt('На сколько оцените его?', '');
+    if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+        personalMovieDB.movies[a] = b;
+        console.log('done');
+    } else {
+        console.log('error');
+        i--;
+    }
+
+
+}

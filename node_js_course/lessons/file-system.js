ef018762ec1107e7b3b61e31d8178bccc3128d48
Doonkan <michayloff@gmail.com>
2023-02-10 22:38:28 +0500

    writeFile2

--- node_js_course/lessons/file-system.js
+++ node_js_course/lessons/file-system.js
@@ -20,16 +20,51 @@ const path = require('path');
 //     }
 // });
 
-fs.writeFile(path.resolve(__dirname, 'test.txt'), 'fdoijnvoidfnvndofinv', (err) => {
-    if(err) {
-        throw err;
-    }
-    console.log('Файл записан');
-});
+// fs.writeFile(path.resolve(__dirname, 'test.txt'), '111 222 3333 fdoijnvoidfnvndofinv', (err) => {
+//     if(err) {
+//         throw err;
+//     }
+//     console.log('Файл записан');
+// });
+//
+// fs.appendFile(path.resolve(__dirname, 'test.txt'), '  Добавить в конец файла', (err) => {
+//     if(err) {
+//         throw err;
+//     }
+//     console.log('Файл записан');
+// });
 
-fs.appendFile(path.resolve(__dirname, 'test.txt'), '  Добавить в конец файла', (err) => {
-    if(err) {
-        throw err;
-    }
-    console.log('Файл записан');
-});
+const writeFileAsync = async (path, data) => {
+    return new Promise((resolve, reject) => fs.writeFile(path, data, (err) => {
+        if(err) {
+            return reject(err.message)
+        }
+        resolve();
+    }));
+};
+
+const appendFileAsync = async (path, data) => {
+    return new Promise((resolve, reject) => fs.appendFile(path, data, (err) => {
+        if(err) {
+            return reject(err.message);
+        }
+        resolve();
+    }));
+};
+
+const readFileAsync = async (path) => {
+    return new Promise((resolve, reject) => fs.readFile(path, {encoding: 'utf-8'}, (err, data) => {
+        if(err) {
+            return reject(err.message);
+        }
+        resolve(data);
+    }));
+};
+
+writeFileAsync(path.resolve(__dirname, 'test.txt'), 'data')
+    .then(() => appendFileAsync(path.resolve(__dirname, 'test.txt'), "343"))
+    .then(() => appendFileAsync(path.resolve(__dirname, 'test.txt'), "4454556"))
+    .then(() => appendFileAsync(path.resolve(__dirname, 'test.txt'), "223578"))
+    .then(() => readFileAsync(path.resolve(__dirname, 'test.txt')))
+    .then(data => console.log(data))
+    .catch(err => console.log(err));

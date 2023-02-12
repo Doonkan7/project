1ae56e1607e634dac29942c13843c764c0ca725b
Doonkan <michayloff@gmail.com>
2023-02-12 19:26:06 +0500

    index-0.1

--- node_js_course/index.js
+++ node_js_course/index.js
@@ -1,7 +1,6 @@
 const http = require('http');
 const EventEmitter = require('events');
 const PORT = process.env.PORT || 5000;
-
 const emitter = new EventEmitter();
 
 class Router {
@@ -9,14 +8,14 @@ class Router {
         this.endpoints = {};
     }
 
-    request(method = "GET", path, handler) {
-        if(!this.endpoints[path]) = {
-            this.endpoints[path] = {}
+    request(method = 'GET', path, handler) {
+        if(!this.endpoints[path]) {
+            this.endpoints[path] = {};
         }
         const endpoint = this.endpoints[path];
 
         if(endpoint[method]) {
-            throw new Error(`[${method}] по адресу ${path} уже существует маршрут`);
+            throw new Error(`[${method}] по адресу ${path} уже существует`);
         }
 
         endpoint[method] = handler;
@@ -25,22 +24,21 @@ class Router {
         });
     }
 
-    get(path, handler) {
+    get(path,handler) {
         this.request('GET', path, handler);
     }
-    post(path, handler) {
+    post(path,handler) {
         this.request('POST', path, handler);
     }
-    put(path, handler) {
+    put(path,handler) {
         this.request('PUT', path, handler);
     }
-    delete(path, handler) {
+    delete(path,handler) {
         this.request('DELETE', path, handler);
     }
 }
 
 const router = new Router();
-
 router.get('/users', (req, res) => {
     res.end('YOU SEND REQUEST TO /USERS');
 });
@@ -50,18 +48,7 @@ router.get('/posts', (req, res) => {
 });
 
 const server = http.createServer((req, res) => {
-    emitter.emit(`[${req.url}]:[${req.method}]`, req, res)
-    res.writeHead(200, {
-        'Content-type':  'application/json'
-    });
-    if(req.url === '/users'){
-        return res.end(JSON.stringify([
-            {id: 1, name: 'Ulbi TV'}
-        ]));
-    }
-    if(req.url === '/posts'){
-        return res.end('POSTS');
-    }
+    emitter.emit(`[${req.url}]:[${req.method}]`, req, res);
     res.end(req.url);
 });
 

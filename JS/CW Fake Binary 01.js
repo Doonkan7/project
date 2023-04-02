b6500afe04dcdbb3ed1d697e295328a37bd8cc4a
Doonkan <michayloff@gmail.com>
2023-04-02 20:44:34 +0500

    CodeWars

--- JS/CW Fake Binary 01.js
+++ JS/CW Fake Binary 01.js
@@ -12,3 +12,14 @@ function fakeBin(x){
 
 console.log(fakeBin('45385593107843568'));
 
+// 2)
+
+function fakeBin(x) {
+    return x.split('').map(n => n < 5 ? 0 : 1).join('');
+}
+
+// 3)
+
+function fakeBin(x) {
+    return x.replace(/\d/g, d => d < 5 ? 0 : 1);
+}

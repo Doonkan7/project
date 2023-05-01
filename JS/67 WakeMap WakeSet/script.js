8339a00e5298f52576f9963d1960357ca7a4950d
Doonkan <michayloff@gmail.com>
2023-05-01 23:16:27 +0500

    71

---
+++ JS/67 WakeMap WakeSet/script.js
@@ -0,0 +1,46 @@
+// let user = {name: 'Ivan'};
+//
+// let map = new WeakMap();
+// map.set(user, 'data');
+//
+// user = null;
+//
+// console.log(map.has(user));
+
+
+let cache = new WeakMap();
+
+function cacheUser(user) {
+    if (!cache.has(user)) {
+        cache.set(user, Date.now());
+}
+    return cache.get(user);
+}
+
+let lena = {name: 'Elena'};
+let  alex = {name: 'Alex'};
+
+cacheUser(lena);
+cacheUser(alex);
+
+lena = null;
+
+console.log(cache.has(lena));
+console.log(cache.has(alex));
+
+let messages = [
+    {text: 'Hello', from: 'John'},
+    {text: 'World', from: 'Alex'},
+    {text: '....', from: 'M'}
+];
+
+let readMessages = new WeakSet();
+
+readMessages.add(messages[0]);
+readMessages.add(messages[1]);
+
+console.log(readMessages.has(messages[0]));
+
+//readMessages.add(messages[0]);
+messages.shift();
+console.log(readMessages.has(messages[0]));
